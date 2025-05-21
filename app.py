1a2e9ab75567b064627b32b8c5f7056c3f4a1efb
Destiani Intan C <62420925+destianiic@users.noreply.github.com>
2025-05-21 14:13:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from huggingface_hub import hf_hub_download
 import joblib
 import numpy as np
 
@@ -8,11 +9,13 @@ import numpy as np
 MAX_SEQUENCE_LENGTH = 250
 class_labels = ['Negative', 'Neutral', 'Positive']
 
-# Load model and tokenizer
+# Load model and tokenizer from Hugging Face Hub
 @st.cache_resource
 def load_model_and_tokenizer():
-    model = load_model("model/model.h5")
-    tokenizer = joblib.load("model/tokenizer.pkl")
+    model_path = hf_hub_download(repo_id="destianiic/lstm-model", filename="model.keras")
+    tokenizer_path = hf_hub_download(repo_id="destianiic/lstm-model", filename="tokenizer.pkl")
+    model = load_model(model_path)
+    tokenizer = joblib.load(tokenizer_path)
     return model, tokenizer
 
 model, tokenizer = load_model_and_tokenizer()
